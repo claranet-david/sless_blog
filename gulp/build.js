f3a5b44029104833bd59054cb70b59fb8168bbc5
David Rivera <thabeat@gmail.com>
2017-03-06 01:23:46 +0100

    LoginFB redirects solved

--- gulp/build.js
+++ gulp/build.js
@@ -19,7 +19,7 @@ gulp.task('partials', function () {
       quotes: true
     }))
     .pipe($.angularTemplatecache('templateCacheHtml.js', {
-      module: 'yoProjectJS',
+      module: 'sless_blog',
       root: 'app'
     }))
     .pipe(gulp.dest(conf.paths.tmp + '/partials/'));
